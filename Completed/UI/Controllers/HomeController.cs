013ffd0e7edb337e6f6731aa37dfbe4271b32646
Krikke21 <christof.lauriers@euri.com>
2019-05-25 22:49:23 +0200

    UI development

--- Completed/UI/Controllers/HomeController.cs
+++ Completed/UI/Controllers/HomeController.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using UI.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace UI.Controllers
 {
@@ -13,31 +7,6 @@ namespace UI.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ Completed/UI/wwwroot/js/services/command_service.js
@@ -0,0 +1,37 @@
+﻿(function() {
+    "use strict";
+
+    var module = angular.module('dice-game');
+
+    module.service('commandService', function($http) {
+
+
+        var baseUrl = "http://localhost:5001/api/";
+
+        return {
+            createGame: function(success, error) {
+            
+                $http.post(baseUrl + 'game/create')
+                    .then(function(response) {
+                        success(response);
+                    }, function(resp){
+                        error();
+                    });
+            },
+            startGame: function(gameId, playersCount, error) {
+                var players = []
+                for (i = 0; i < playersCount; i++) { 
+                    players[i] = 'player' + i;
+                }
+
+                $http.post(baseUrl + '/game/start', { GameId : gameId, Players: players })
+                     .error(error);
+            },
+            roll: function(gameId, player, error) {
+                $http.post(baseUrl + '/game/roll', { GameId : gameId, Player: player })
+                     .error(error);
+            }
+        };
+    });
+
+})();
